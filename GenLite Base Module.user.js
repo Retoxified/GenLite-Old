88f7d91739bedeb275ed8275d1957f6de2287c3a
Retoxified <wesley@nextgensoftware.nl>
2022-12-05 11:51:21 +0100

    GenLite 0.1
    NPC Names, Ground Item Names & Camera unlock

---
+++ GenLite Base Module.user.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name         GenLite Base Module
+// @namespace    GenLite
+// @version      0.1
+// @description  try to take over the world!
+// @author       TwistedFate#4053
+// @match        https://play.genfanad.com/play/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=genfanad.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    class GenLite {
+        moduleList = [];
+
+        init() {
+            this.installHook(Camera.prototype, 'update', this.hook_Camera_update, this);
+            this.installHook(Network.prototype, 'logoutOK', this.hook_Network_logoutOK, this);
+            this.installHook(PhasedLoadingManager.prototype, 'start_phase',  this.hook_PhasedLoadingManager_start_phase,  this);
+        }
+
+        hook_Camera_update() {
+            for (var i = 0; i < this.moduleList.length; i++) {
+                if(typeof this.moduleList[i].update === 'function') {
+                    this.moduleList[i].update.apply(this.moduleList[i], arguments);
+                }
+            }
+        }
+
+        hook_Network_logoutOK() {
+            for (var i = 0; i < this.moduleList.length; i++) {
+                if(typeof this.moduleList[i].logoutOK === 'function') {
+                    this.moduleList[i].logoutOK.apply(this.moduleList[i], arguments);
+                }
+            }
+        }
+
+        hook_PhasedLoadingManager_start_phase(phase) {
+            if(phase === "game_loaded") {
+                for (var i = 0; i < this.moduleList.length; i++) {
+                    if(typeof this.moduleList[i].loginOK === 'function') {
+                        this.moduleList[i].loginOK.apply(this.moduleList[i], arguments);
+                    }
+                }
+            }
+        }
+
+        registerModule(module) {
+            this.moduleList[this.moduleList.length] = module;
+        }
+
+        unregisterModule(module) {
+            for (var i = 0; i < this.moduleList.length; i++) {
+                if(this.moduleList[i] === module) {
+                    this.moduleList.splice(i, 1);
+                    break;
+                }
+            }
+        }
+
+        installHook(object, functionName, callback, callback_this) {
+            (function(originalFunction) {
+                object[functionName] = function () {
+                    var returnValue = originalFunction.apply(this, arguments);
+                    if(callback !== undefined) {
+                        callback.apply(callback_this ?? this, arguments);
+                    }
+
+                    return returnValue;
+                };
+            }(object[functionName]));
+        }
+    }
+
+    window.genlite = new GenLite();
+    window.genlite.init();
+})();
---
+++ GenLite Item Highlight.user.js
@@ -0,0 +1,142 @@
+// ==UserScript==
+// @name         GenLite Item Highlight
+// @namespace    GenLite
+// @version      0.1
+// @description  try to take over the world!
+// @author       TwistedFate#4053
+// @match        https://play.genfanad.com/play/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=genfanad.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    class GenLiteItemHighlight {
+        trackedItems = {};
+		item_highlight_div = null;
+        render = false;
+
+		init() {
+			window.genlite.registerModule(this);
+
+			this.item_highlight_div = document.createElement( 'div' );
+			this.item_highlight_div.className = 'item-indicators-list';
+			document.body.appendChild(this.item_highlight_div);
+		}
+
+        update(dt) {
+            if(this.render == false) {
+                return;
+            }
+
+			let stack_counter = {};
+            let itemsToAdd = Object.keys(GAME.items).filter( x => !Object.keys(this.trackedItems).includes(x) );
+            let itemsToRemove = Object.keys(this.trackedItems).filter( x => !Object.keys(GAME.items).includes(x) );
+
+            for(let key in itemsToAdd) {
+				this.trackedItems[itemsToAdd[key]] = this.create_text_element(itemsToAdd[key], GAME.items[itemsToAdd[key]]);
+			}
+
+			for(let key in itemsToRemove) {
+				this.trackedItems[itemsToRemove[key]].remove();
+				delete this.trackedItems[itemsToRemove[key]];
+			}
+
+			for(let key in this.trackedItems) {
+				if(GAME.items[key] !== undefined) {
+					let posKey = GAME.items[key].pos2.x+','+GAME.items[key].pos2.y;
+					if(stack_counter[posKey] === undefined) {
+						stack_counter[posKey] = 0;
+					}
+
+					let worldPos = new THREE.Vector3().copy(GAME.items[key].position());
+					worldPos.y += 0.5;
+					let screenPos = this.world_to_screen(worldPos, stack_counter[posKey]);
+                    if(screenPos.z > 1.0) {
+                        this.trackedItems[key].style.visibility = 'hidden'; // Behind camera, hide
+                    } else {
+                        this.trackedItems[key].style.visibility = 'visible'; // In front of camera, show
+                    }
+					this.trackedItems[key].style.left = screenPos.x + "px";
+					this.trackedItems[key].style.top = screenPos.y + "px";
+
+					stack_counter[posKey]++;
+				}
+			}
+        }
+
+        loginOK() {
+            this.render = true;
+        }
+        logoutOK() {
+            this.item_highlight_div.innerHTML = '';
+            this.trackedItems = {};
+            this.render = false;
+        }
+
+		world_to_screen(pos, stack_count) {
+			var p = pos;
+			var screenPos = p.project(GRAPHICS.threeCamera());
+
+			screenPos.x = (screenPos.x + 1) / 2 * window.innerWidth;
+			screenPos.y = -(screenPos.y - 1) / 2 * window.innerHeight - (stack_count * 15);
+
+			return screenPos;
+		}
+
+        getItemValue(item) {
+            let itemDefId = item.definition.item;
+            let quantity = item.definition.quantity ?? 1;
+            let gameValue = DATA.items[itemDefId].value ?? 1;
+            return gameValue * quantity;
+        }
+
+        getItemColor(item) {
+            let itemValue = this.getItemValue(item);
+
+            if(itemValue >= 10000) {
+                return 'text-ran';
+            } else if(itemValue >= 5000) {
+                return 'text-purple';
+            } else if(itemValue >= 1000) {
+                return 'text-magenta';
+            } else if(itemValue >= 500) {
+                return 'text-gold';
+            } else if(itemValue >= 100) {
+                return 'text-limegreen';
+            } else {
+                return 'text-white';
+            }
+        }
+
+		create_text_element(key, item) {
+			let element = document.createElement( 'div' );
+			element.className = this.getItemColor(item);
+			element.style.position = 'absolute';
+			//element.style.zIndex = '99999';
+			element.innerHTML = item.item_name;
+            let stackable = DATA.items[item.definition.item].stackable ?? false;
+            if(stackable === true) {
+                element.innerHTML = `${item.item_name}(${item.definition.quantity})`;
+            }
+			element.style.transform = 'translateX(-50%)';
+            element.style.textShadow = '-1px -1px 0 #000,0   -1px 0 #000, 1px -1px 0 #000, 1px  0   0 #000, 1px  1px 0 #000, 0    1px 0 #000, -1px  1px 0 #000, -1px  0   0 #000';
+            element.style.pointerEvents = 'none';
+
+			this.item_highlight_div.appendChild(element);
+
+			return element;
+		}
+    }
+
+    window.genliteItemHighlight = new GenLiteItemHighlight();
+
+    let gameLoadTimer = setInterval(function() {
+        try {
+            if(window.genlite !== undefined) {
+				window.genliteItemHighlight.init();
+                clearInterval(gameLoadTimer);
+            }
+        } catch (e) {
+        }
+    }, 1000);
+})();
