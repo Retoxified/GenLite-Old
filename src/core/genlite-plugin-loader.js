e2f5cc83db8b35710ab9ef05defd1af6a11530b7
Xortrox <andy@furious.no>
2022-12-19 17:26:42 +0100

    Added genlite-plugin-loader.js whose responsibility is loading and verifying plugin structures

---
+++ src/core/genlite-plugin-loader.js
@@ -0,0 +1,60 @@
+export class GenLitePluginLoader {
+    plugins;
+
+    constructor() {
+        this.plugins  = [];
+    }
+
+    /**
+     * Instantiates an instance of pluginClass,
+     * Runs its async init() function
+     * Adds a window field for the instance matching pluginClass.name
+     * The plugin instance will then also appear in this.plugins[]
+     * @param pluginClass
+     * @returns {Promise<boolean>}
+     */
+    async addPlugin(pluginClass) {
+        try {
+            this.verifyPluginClassStructure(pluginClass);
+        } catch (e) {
+            console.error('[GenLitePluginLoader]: Error loading plugin:', e);
+            console.error('[GenLitePluginLoader]: Plugin classes need to follow the given example structure', this.getExampleStructure());
+            return false;
+        }
+
+        const pluginInstance = new pluginClass();
+        await pluginInstance.init();
+
+        window[pluginClass.name] = pluginInstance;
+
+        this.plugins.push(pluginInstance);
+        console.log(`[GenLitePluginLoader]: Loaded plugin ${pluginClass}`);
+
+        return true;
+    }
+
+    /**
+     * Throws errors if pluginClass is missing specific fields, such as init
+     * @param pluginClass
+     */
+    verifyPluginClassStructure(pluginClass) {
+        if (!pluginClass.__proto__.init) {
+            throw new Error(`Plugin class ${pluginClass} does not define an init function.`);
+        }
+
+        if (typeof(pluginClass.__proto__.init) !== 'function') {
+            throw new Error(`Plugin class ${pluginClass}.init was not defined as a function`);
+        }
+
+        if (!pluginClass.__proto__.name) {
+            throw new Error(`Plugin class ${pluginClass} does not define a name`);
+        }
+    }
+
+    getExampleStructure() {
+        return class ExamplePlugin {
+            static name = 'My Plugin';
+            async init() {}
+        }
+    }
+}
