f9324fb0eef92be55413be9f471e3706b8030eb0
snwhd <>
2023-02-24 23:34:54 -0800

    fix itemstack interface

--- src/core/interfaces/item.interface.ts
+++ src/core/interfaces/item.interface.ts
@@ -1,56 +1,58 @@
-interface Location {
-  position: {
-    x: number;
-    y: number;
-  }
+interface ILocation {
+    position: {
+        x: number;
+        y: number;
+    }
 }
 
-interface ItemInfo {
-  ids: { [id: string]: boolean };
-  value: number;
-  name: string;
-  text(): string;
-  examine: string;
+interface IItemInfo {
+    ids: { [id: string]: boolean };
+    value: number;
+    name: string;
+    text(): string;
+    examine: string;
 }
 
-interface Sprite {
-  sprite: any; // THREE.Sprite;
-  ids: { [id: string]: boolean };
+interface ISprite {
+    sprite: any; // THREE.Sprite;
+    ids: { [id: string]: boolean };
 }
 
-interface ItemKeys {
-  [id: string]: {
-    image: string;
-    item_id: string;
-  }
+interface IItemKeys {
+    [id: string]: {
+        image: string;
+        item_id: string;
+    }
 }
 
-interface Action {
-  color: string;
-  distance: number;
-  priority: number;
-  object: ItemInfo;
-  text: string;
-  action: () => void;
+interface IAction {
+    color: string;
+    distance: number;
+    priority: number;
+    object: IItemInfo;
+    text: string;
+    action: () => void;
 }
 
-interface ItemStack {
-  id: string;
-  location: Location;
-  dirty: boolean;
-  item_keys: ItemKeys;
-  sprites: { [image: string]: Sprite };
-  item_info: { [item_id: string]: ItemInfo };
-  optimizationX: number;
-  optimizationY: number;
-  ignore_intersections: boolean;
-  mesh: any; // THREE.Group;
-  worldPos: { x: number; y: number; z: number };
-  position(): { x: number; y: number; z: number };
-  text(): string;
-  actions(): Action[];
-  addInstance(id: string, def: { item: string }): void;
-  removeInstance(id: string): boolean;
-  intersects(ray: any /*THREE.Raycaster*/, list: Action[]): void;
-  update(cam: any /*THREE.Camera*/, dt: number): void;
+interface IItemStack {
+    id: string;
+    location: ILocation;
+    dirty: boolean;
+    item_keys: IItemKeys;
+    sprites: { [image: string]: ISprite };
+    item_info: { [item_id: string]: IItemInfo };
+    optimizationX: number;
+    optimizationY: number;
+    ignore_intersections: boolean;
+    mesh: any; // THREE.Group;
+    worldPos: { x: number; y: number; z: number };
+    position(): { x: number; y: number; z: number };
+    text(): string;
+    actions(): IAction[];
+    addInstance(id: string, def: { item: string }): void;
+    removeInstance(id: string): boolean;
+    intersects(ray: any /*THREE.Raycaster*/, list: IAction[]): void;
+    update(cam: any /*THREE.Camera*/, dt: number): void;
+
+    prototype: any;
 }
