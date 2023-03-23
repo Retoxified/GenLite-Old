46ce874cd19a65f7938e009d98f331d27a4179e3
KKonaOG <kylerfarrar.gaming@gmail.com>
2023-03-23 14:03:26 -0500

    Fix: NPC Highlights innerHTML undefined bug fixed

--- src/plugins/genlite-npc-highlight.plugin.ts
+++ src/plugins/genlite-npc-highlight.plugin.ts
@@ -198,8 +198,9 @@ export class GenLiteNPCHighlightPlugin implements GenLitePlugin {
         npcsToMod = Object.keys(document.game.GAME.npcs).filter(x => document.game.GAME.npcs[x].id.split('-')[0] == object.id.split('-')[0]);
         for (let key in npcsToMod) {
             let npcid = npcsToMod[key];
-            if (this.trackedNpcs[npcid] && this.trackedNpcs[npcid].hasHp)
+            if ((this.trackedNpcs[npcid] && this.trackedNpcs[npcid].hasHp) || this.trackedNpcs[npcid] === undefined)
                 continue;
+
             this.trackedNpcs[npcid].innerHTML += ` HP: ${this.npcHealthList[hpKey]}`;
             this.trackedNpcs[npcid].hasHp = true;
         }
