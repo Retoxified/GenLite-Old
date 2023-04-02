c9efb2114c65e2320f632f792b9cc1aaeeeae7e4
snwhd <>
2023-04-01 23:57:53 -0700

    UI transition

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,7 @@ import { GenLiteHealthRegenerationPlugin } from './plugins/genlite-health-regene
 import { GenLiteFPSCounter } from "./plugins/genlite-fps.plugin";
 import { GenLiteEnhancedContextMenu } from "./plugins/genlite-enhanced-context-menu.plugin";
 import { GenLiteQuestPlugin } from "./plugins/genlite-quest.plugin";
+import { GenLiteResizePlugin } from "./plugins/genlite-resize.plugin";
 import { GenLiteEnhancedBanking } from "./plugins/genlite-enhanced-banking.plugin";
 
 declare const GM_getResourceText: (s: string) => string;
@@ -228,6 +229,7 @@ let isInitialized = false;
         await genlite.pluginLoader.addPlugin(GenLiteFPSCounter);
         await genlite.pluginLoader.addPlugin(GenLiteEnhancedContextMenu);
         await genlite.pluginLoader.addPlugin(GenLiteQuestPlugin);
+        await genlite.pluginLoader.addPlugin(GenLiteResizePlugin);
         await genlite.pluginLoader.addPlugin(GenLiteEnhancedBanking);
 
         /** post init things */
