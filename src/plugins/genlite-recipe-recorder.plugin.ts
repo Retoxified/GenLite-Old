a12d8ef7c3c4c044c022fe8db8f0538780f7f317
snwhd <>
2023-04-08 15:53:23 -0700

    recipe recorder ui

--- src/plugins/genlite-recipe-recorder.plugin.ts
+++ src/plugins/genlite-recipe-recorder.plugin.ts
@@ -32,6 +32,8 @@ export class GenLiteRecipeRecorderPlugin extends GenLitePlugin {
 
     isPluginEnabled: boolean = false;
 
+    uiTab: HTMLElement = null;
+
     async init() {
         document.genlite.registerPlugin(this);
 
@@ -44,14 +46,229 @@ export class GenLiteRecipeRecorderPlugin extends GenLitePlugin {
             this.recipeResults = saved.recipe ? saved.recipe : {};
             this.gatherResults = saved.gathering ? saved.gathering : {};
         }
+
+        this.createCSS();
+        this.createUITab();
     }
 
     async postInit() {
         document.genlite.ui.registerPlugin("Recipe Recorder", null, this.handlePluginState.bind(this));
     }
 
+    createCSS() {
+        const style = document.createElement('style');
+        style.innerHTML = `
+            .genlite-recipes-container {
+                display: flex;
+                flex-direction: column;
+                overflow-x: hidden;
+                color: #ffd593;
+                font-family: acme,times new roman,Times,serif;
+                row-gap: 1em;
+                padding: 1em;
+                height: 100%;
+            }
+
+            .genlite-recipes-list {
+                display: flex;
+                flex-direction: column;
+                row-gap: 1em;
+                overflow-y: scroll;
+                height: 80%;
+            }
+
+            .genlite-recipes-row {
+                display: flex;
+                flex-direction: column;
+                background-color: #0e0c0b;
+                padding: 0.25em;
+                flex-shrink: 0;
+            }
+
+            .genlite-recipes-iconlist {
+                display: flex;
+                column-gap: 0.5em;
+                padding: 0.25em;
+                overflow-x: scroll;
+            }
+
+            .genlite-recipes-arrow {
+                width: 28px;
+                height: 28px;
+                cursor: pointer;
+                display: flex;
+                align-items: center;
+            }
+
+            .genlite-recipes-arrow i {
+                margin: auto;
+            }
+
+            .genlite-recipes-icon {
+                width: 28px;
+                height: 28px;
+                position: relative;
+            }
+
+            .genlite-recipes-output {
+                display: none;
+                flex-direction: column;
+            }
+
+            .genlite-recipes-output-row {
+                display: flex;
+                column-gap: 1em;
+            }
+
+            .genlite-recipes-search {
+            }
+        `;
+        document.head.appendChild(style);
+    }
+
+    createUITab() {
+        if (this.uiTab) {
+            this.uiTab.remove();
+        }
+
+        let settingsMenu = <HTMLElement>document.createElement("div");
+        settingsMenu.classList.add("genlite-recipes-container");
+
+        // search bar
+        let search = <HTMLInputElement>document.createElement("input");
+        settingsMenu.appendChild(search);
+        search.classList.add("genlite-recipes-search");
+        search.type = "text";
+
+        search.oninput = function (e) {
+            let value = search.value.trim();
+            let list = document.getElementsByClassName("genlite-recipes-row");
+            for (let i = 0; i < list.length; i++) {
+                let row = list[i] as HTMLElement;
+                let content = row.innerHTML.toLowerCase();
+                if (value === "") {
+                    row.style.removeProperty("display");
+                } else if (content.includes(value)) {
+                    row.style.removeProperty("display");
+                } else {
+                    row.style.display = "none";
+                }
+            }
+        };
+
+        // recipe list
+        let listContainer = <HTMLElement>document.createElement("div");
+        listContainer.classList.add("genlite-recipes-list");
+        settingsMenu.appendChild(listContainer);
+        for (const recipeName in this.recipeResults) {
+            const result = this.recipeResults[recipeName];
+
+            let row = <HTMLElement>document.createElement("div");
+            row.classList.add("genlite-recipes-row");
+            listContainer.appendChild(row);
+
+            // inputs
+            let icons = <HTMLElement>document.createElement("div");
+            icons.classList.add("genlite-recipes-iconlist");
+            row.appendChild(icons);
+
+            let arrow = <HTMLElement>document.createElement("div");
+            arrow.classList.add("genlite-recipes-arrow");
+            let i = <HTMLElement>document.createElement("i");
+            i.classList.add("fa-chevron-right");
+            i.classList.add("fas");
+            arrow.appendChild(i);
+            icons.appendChild(arrow);
+
+            let nInputs = 0;
+            for (const item in result.input) {
+                nInputs = result.input[item]; // TODO: make sure input #s are always equal
+                let div = <HTMLImageElement>document.createElement("div");
+                div.classList.add("genlite-recipes-icon");
+                icons.appendChild(div);
+
+                let icon = <HTMLImageElement>document.createElement("img");
+                icon.classList.add("genlite-recipes-icon");
+                icon.title = item;
+                div.appendChild(icon);
+
+                const itemdata = document.game.DATA.items[item];
+                if (itemdata && itemdata.image && itemdata.name) {
+                    icon.title = itemdata.name;
+                    icon.src = document.game.getStaticPath('items/' + itemdata.image);
+
+                    if (itemdata.border) {
+                        let path = `items/placeholders/${ itemdata.border }_border.png`;
+                        path = document.game.getStaticPath(path);
+                        let qual = <HTMLImageElement>document.createElement("img");
+                        qual.classList.add("new_ux-inventory_quality-image");
+                        qual.src = path;
+                        div.appendChild(qual);
+                    }
+                }
+            }
+
+            // now draw outputs
+            let outputBox = <HTMLElement>document.createElement("div");
+            outputBox.classList.add("genlite-recipes-output");
+            row.appendChild(outputBox);
+            outputBox.appendChild(document.createTextNode(`${nInputs} tries`));
+
+            for (const item in result.output) {
+                let orow = <HTMLElement>document.createElement("div");
+                orow.classList.add("genlite-recipes-output-row");
+
+                let div = <HTMLImageElement>document.createElement("div");
+                div.classList.add("genlite-recipes-icon");
+                orow.appendChild(div);
+
+                let icon = <HTMLImageElement>document.createElement("img");
+                icon.classList.add("genlite-recipes-icon");
+                icon.title = item;
+                div.appendChild(icon);
+
+                const itemdata = document.game.DATA.items[item];
+                if (itemdata && itemdata.image && itemdata.name) {
+                    icon.title = itemdata.name;
+                    icon.src = document.game.getStaticPath('items/' + itemdata.image);
+
+                    if (itemdata.border) {
+                        let path = `items/placeholders/${ itemdata.border }_border.png`;
+                        path = document.game.getStaticPath(path);
+                        let qual = <HTMLImageElement>document.createElement("img");
+                        qual.classList.add("new_ux-inventory_quality-image");
+                        qual.src = path;
+                        div.appendChild(qual);
+                    }
+                }
+
+                let n = result.output[item];
+                let pct = (n / nInputs * 100);
+                pct = Math.round(pct * 100) / 100;
+                orow.appendChild(document.createTextNode(`${n} (${pct}%)`));
+
+                outputBox.appendChild(orow);
+            }
+
+            arrow.onclick = function (e) {
+                if (i.classList.toggle("fa-chevron-right")) {
+                    i.classList.remove("fa-chevron-down");
+                    outputBox.style.display = "none";
+                } else {
+                    i.classList.add("fa-chevron-down");
+                    outputBox.style.display = "flex";
+                }
+            };
+        }
+
+        this.uiTab = document.genlite.ui.addTab("kitchen-set", "Recipe Recorder", settingsMenu, this.isPluginEnabled);
+    }
+
     handlePluginState(state: boolean): void {
         this.isPluginEnabled = state;
+        if (this.uiTab) {
+            this.uiTab.style.display = state ? "flex" : "none";
+        }
     }
 
     Network_logoutOK() {
