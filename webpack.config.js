f52c668e4c21a48fcdef6d5542c0d209534673a3
dpe <mattmrad62@gmail.com>
2023-04-13 21:09:45 -0400

    QWERTY

--- webpack.config.js
+++ webpack.config.js
@@ -129,7 +129,7 @@ module.exports = (env, argv) => {
     repoOwner = configStuff.repository_owner;
   } else {
     repoOwner = env.repoOwner;
-    console.log("repo owner", process.repoOwner)
+    console.log("repo owner", process.env.repoOwner)
   }
   let githubConfig = {};
   if (env.type == "release") {
